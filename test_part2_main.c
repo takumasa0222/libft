3d3d1ed1220a7cca100de4c1aa2475ec9d97acf4
Takumasa Matsuura <tamatsuu@student.42tokyo.jp>
2024-05-13 23:35:56 +0900

    update test code

--- test_part2_main.c
+++ test_part2_main.c
@@ -6,7 +6,7 @@
 /*   By: tamatsuu <tamatsuu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:49:19 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/05/12 21:20:01 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2024/05/13 23:25:58 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	succuss()
 
 void	fail()
 {
-	printf("\x1b[31mNG x1b[39m\n");
+	printf("\x1b[31mNG \x1b[39m");
 }
 
 void	test_substr()
@@ -61,15 +61,156 @@ void	test_substr()
 		succuss();
 	else
 		fail();
+	printf("\n");
 }
 
 void	test_strjoin()
 {
-	t1 char
+	printf("strjoin\n");
+	char	*t1 = "Hello";
+	char	*t2 = "World";
+	char	*t3;
+
+	t3 = ft_strjoin(t1, t2);
+	if (strlen(t3) == 10 && !strcmp(t3, "HelloWorld"))
+		succuss();
+	else
+		fail();
+	t3 = ft_strjoin(t1, NULL);
+	if (strlen(t3) == 5 && !strcmp(t3, "Hello"))
+		succuss();
+	else
+		fail();
+	t3 = ft_strjoin(NULL, t2);
+	if (strlen(t3) == 5 && !strcmp(t3, "World"))
+		succuss();
+	else
+		fail();
+	t3 = ft_strjoin("", "");
+	if (strlen(t3) == 0 && !strcmp(t3, ""))
+		succuss();
+	else
+		fail();
+	printf("\n");
+}
+void	test_strtrim()
+{
+	char *ret;
+	printf("strtrim\n");
+	ret = ft_strtrim("aabbcchelloworldccbbaa", "abc");
+	if (!strcmp(ret, "helloworld"))
+		succuss();
+	else
+		fail();
+
+	ret = ft_strtrim("aaBbcchelloworldccbbaa", "abc");
+	if (!strcmp(ret, "Bbcchelloworld"))
+		succuss();
+	else
+		fail();
+	ret = ft_strtrim("aabbcchelloworldccbbaa", "abc");
+	if (!strcmp(ret, "helloworld"))
+		succuss();
+	else
+		fail();
+	ret = ft_strtrim("aabbcchelloworldccDbaa", "abc");
+	if (!strcmp(ret, "helloworldccD"))
+		succuss();
+	else
+		fail();
+	ret = ft_strtrim("aabbcchelloworldccbbaa", "");
+	if (!strcmp(ret, "aabbcchelloworldccbbaa"))
+		succuss();
+	else
+		fail();
+	ret = ft_strtrim("", "abc");
+	if (!strcmp(ret, ""))
+		succuss();
+	else
+		fail();
+	ret = ft_strtrim(NULL, "abc");
+	if (ret == NULL)
+		succuss();
+	else
+		fail();
+	ret = ft_strtrim("hello", NULL);
+	if (ret == NULL)
+		succuss();
+	else
+		fail();
+	printf("\n");
+}
+
+void	test_split()
+{
+	printf("split\n");
+	char	**ret;
+	char	*test[5] = {"He", "owor", "d", NULL};
+	int		i = 0;
+	ret = ft_split("Helloworld", 'l');
+	while (ret[i] != NULL)
+	{
+		if (strcmp(ret[i], test[i]))
+			fail();
+		i++;
+	}
+	if (i == 3)
+		succuss();
+	char	*test1[5] = {"HeWord", NULL};
+	i = 0;
+	ret = ft_split("HeWord", 'l');
+	while (ret[i] != NULL)
+	{
+		if (strcmp(ret[i], test1[i]))
+			fail();
+		i++;
+	}
+	if (i == 1)
+		succuss();
+
+	char	*test2[5] = {NULL};
+	i = 0;
+	ret = ft_split(NULL, 'l');
+	if (i == 0 && ret == NULL)
+		succuss();
+
+	char	*test3[5] = {"HelloWord", NULL};
+	i = 0;
+	ret = ft_split("HelloWord", '\0');
+	while (ret[i] != NULL)
+	{
+		if (strcmp(ret[i], test3[i]))
+			fail();
+		i++;
+	}
+	if (i == 1)
+		succuss();
+
+	char	*test4[5] = {NULL};
+	i = 0;
+	ret = ft_split("\0HelloWord", '\0');
+	if (i == 0 && ret[i] == NULL)
+		succuss();
+	else
+		fail();
+
+	char	*test5[30] = {"He","ll","o","W","o","r","d","  "," ","   ", NULL};
+	i = 0;
+	ret = ft_split("aaHeaallaaoaWaoaradaaa  a aa   aa", 'a');
+	while (ret[i] != NULL)
+	{
+		if (strcmp(ret[i], test5[i]))
+			fail();
+		i++;
+	}
+	if (i == 10 )
+		succuss();
 }
 
 int	main(void)
 {
-	test_substr();
-	test_strjoin()
+	// test_substr();
+	// test_strjoin();
+	// test_strtrim();
+	test_split();
 }
